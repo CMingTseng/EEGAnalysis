931ed84aaf1d313d3dbd9ed2a536f05d30294592
hisaki.oomae <hisaki0616@gmail.com>
2018-05-29 19:33:55 +0900

    first commit

---
+++ src/main/java/com/hisaki/www/eeg_analysis/EegAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.hisaki.www.eeg_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EegAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EegAnalysisApplication.class, args);
+    }
+}
