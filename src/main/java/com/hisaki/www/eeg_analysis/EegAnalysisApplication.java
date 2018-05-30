6b65b36f7fef9634f92795173364b5701cb74798
hisaki.oomae <hisaki0616@gmail.com>
2018-05-30 13:53:37 +0900

    hello world 分追加

--- src/main/java/com/hisaki/www/eeg_analysis/EegAnalysisApplication.java
+++ src/main/java/com/hisaki/www/eeg_analysis/EegAnalysisApplication.java
@@ -2,11 +2,20 @@ package com.hisaki.www.eeg_analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @SpringBootApplication
 public class EegAnalysisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EegAnalysisApplication.class, args);
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String root() {
+        return "hello world!!";
     }
 }
