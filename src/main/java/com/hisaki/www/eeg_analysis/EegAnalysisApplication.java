b50746301ccec3c49db2f58b43924fcbbfbe2c95
hisaki.oomae <hisaki0616@gmail.com>
2018-05-31 21:16:11 +0900

    delete AuthenticationLibrary

--- src/main/java/com/hisaki/www/eeg_analysis/EegAnalysisApplication.java
+++ src/main/java/com/hisaki/www/eeg_analysis/EegAnalysisApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
@@ -15,9 +15,9 @@ public class EegAnalysisApplication {
         SpringApplication.run(EegAnalysisApplication.class, args);
     }
 
-    @GetMapping("/")
-    @ResponseBody
-    public String root() {
-        return "hello world!!";
-    }
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String root() {
+//        return "hello world!!";
+//    }
 }
